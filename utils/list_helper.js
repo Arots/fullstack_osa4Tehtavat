599af39982ce2d96822a8938c8b7c0bb1fe986e9
Aro <otso.aro@cgi.com>
2018-07-30 14:05:24 +0300

    test changes

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,23 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    const ammount = (sum, blog) => sum + blog.likes;
+
+    return blogs.length ? blogs.reduce(ammount, 0) : 0
+}
+
+const favoriteBlog = (blogs) => {
+    const favorite = blogs.map(blog => blog.likes)
+    const mostLikesObject = blogs.find(blog => 
+        blog.likes === Math.max(...favorite))
+
+
+    return mostLikesObject
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favoriteBlog
 }
