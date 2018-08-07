c3c1e613a815c065172bed062591c9d752d40cf8
Aro <otso.aro@cgi.com>
2018-08-07 09:09:19 +0300

    4.19 done

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,15 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = (request) => {
+	const authorization = request.get('authorization')
+	if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+		return authorization.substring(7)
+	}
+	return null
+}
 
 blogRouter.get('/', async (request, response) => {
 	try {
@@ -19,11 +28,18 @@ blogRouter.post('/', async (request, response) => {
 	try {
 		const body = request.body
 
+		const token = getTokenFrom(request)
+		const decodedToken = jwt.verify(token, process.env.SECRET)
+
+		if (!token || !decodedToken.id) {
+			return response.status(401).json({ error: 'token missing or invalid' })
+		}
+
 		if (request.body.title === undefined || request.body.url === undefined ) {
 			return response.status(400).send({error: 'Bad request'})
 		}
 
-		const user = await User.findById(body.userId)
+		const user = await User.findById(decodedToken.id)
 
 		const blog = new Blog ({
 			title: request.body.title,
@@ -41,8 +57,12 @@ blogRouter.post('/', async (request, response) => {
 		response.status(201).json(Blog.format(savedBlog))
 			
 	} catch (exception) {
-		console.log(exception)
-		response.status(500).send({ error: 'Post didnt work' })
+		if (exception.name === 'JsonWebTokenError' ) {
+			response.status(401).json({ error: exception.message })
+		} else {
+			console.log(exception)
+			response.status(500).json({ error: 'Post didnt work' })
+		}
 	}
 })
 
